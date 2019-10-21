b4a362f0bf1f968fd740defa2407c847bde252cb
Mukund Raghothaman <raghotha@localhost.localdomain>
2019-10-21 04:30:29 -0700

    Updates

--- exp2/run-int.sh
+++ exp2/run-int.sh
@@ -4,4 +4,4 @@ PROBLEM_DIR=$1
 i=$2
 LOG_FILE=$3
 DATA_FILE=$4
-./prosynth/scripts/prosynth $PROBLEM_DIR 0 1 $i $DATA_FILE &> $LOG_FILE
+./prosynth/scripts/prosynth $PROBLEM_DIR 0 1 $DATA_FILE &> $LOG_FILE
