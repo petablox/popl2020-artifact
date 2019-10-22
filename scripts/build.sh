51049a30672280223281658f402c5be99716ccad
Mukund Raghothaman <raghotha@localhost.localdomain>
2019-10-22 02:17:58 -0700

    Updates

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-pushd prosynth/souffle
-    ./bootstrap
-    ./configure
-    make -j 8
+pushd popl2020-artifact
+    pushd prosynth/souffle
+        ./bootstrap
+        ./configure
+        make -j 8
+    popd
+
+    pushd difflog
+        sbt compile
+    popd
+
+    pushd alps
+        make -j 4
+    popd
 popd
 
